9f690104f0792d20e98bc7383459212c5b1f2ac6
Leonardo Resende <leonardo@i4h.com.br>
2022-06-05 23:15:13 -0300

    Navigation styles and logo

---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,25 @@
+import { Fragment } from 'react'
+import { Link, Outlet } from 'react-router-dom'
+
+import './navigation.styles.scss'
+import { ReactComponent as CrwnLogo} from '../../assets/crown.svg'
+
+const Navigation = () => {
+  return (
+    <Fragment>
+      <div className='navigation'>
+        <Link to='/' className='logo-container'>
+          <CrwnLogo className='logo' />
+        </Link>
+        <div className='nav-links-container'>
+          <Link to='shop' className='nav-link'>
+            <div>SHOP</div>
+          </Link>
+        </div>
+      </div>
+      <Outlet />
+    </Fragment>
+  )
+}
+
+export default Navigation
